b28aea8b3b6247e48fed12058bcf8a10bf6943fc
eleves <eleves@mac404inf-15.local>
2017-11-30 12:14:32 -0500

    Version :: 1.0 - Add file

---
+++ evaluation+/UIColorUtil.swift
@@ -0,0 +1,23 @@
+//
+//  UIColorUtil.swift
+//  AddUp
+//
+//  Created by eleves on 2017-10-20.
+//  Copyright © 2017 Mario Geneau. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+        convenience init(red: Int, green: Int, blue: Int) {
+            self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+        }
+        
+        convenience init(rgb: Int) {
+            self.init(
+                red: (rgb >> 16) & 0xFF,
+                green: (rgb >> 8) & 0xFF,
+                blue: rgb & 0xFF
+            )
+        }
+}
